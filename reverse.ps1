2394a3aa1cf1efee9fd1dc49b79324e083fb8a14
TimmyTseng <32336596+qoo7972365@users.noreply.github.com>
2026-01-25 11:08:59 +0800

    Update reverse.ps1

--- reverse.ps1
+++ reverse.ps1
@@ -1,2 +1,4 @@
-"Hello World" | Out-File C:\test.txt
-Test-NetConnection -ComputerName 0.tcp.ap.ngrok.io -Port 11848 | Out-File C:\network_test.txt -Append
+# 僅測試網路連通性，不包含敏感的連線代碼
+$dest = "0.tcp.ap.ngrok.io"
+$port = 11848
+Test-NetConnection -ComputerName $dest -Port $port | Out-File C:\connection_test.txt
